eef3ee848a959c0e5c5859583ac5ecc2106e4263
shinkiruki <wkr1305@naver.com>
2023-04-12 17:20:44 +0900

    study-04-12

--- JavaScript/20230404/005_function.js
+++ JavaScript/20230404/005_function.js
@@ -1,200 +1,192 @@
+//4월 4일 코드 + 맨 아래 4월 12일 코드
+
 // 함수
 
 // 1. 재사용성이 높아집니다.
 function one() {
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
 }
-one()
-one()
-one()
-one()
-one()
+one();
+one();
+one();
+one();
+one();
 
 // 2. 유지보수가 용이합니다.
-땅파기() // 10만줄
-기반다지기() // 10만줄
-기둥세우기() // 10만줄
-벽돌쌓기() // 10만줄
-지붕올리기() // 10만줄
+땅파기(); // 10만줄
+기반다지기(); // 10만줄
+기둥세우기(); // 10만줄
+벽돌쌓기(); // 10만줄
+지붕올리기(); // 10만줄
 
 // 3. 구조 파악이 용이합니다.
-땅파기() // 10만줄
-기반다지기() // 10만줄
-기둥세우기() // 10만줄
-벽돌쌓기() // 10만줄
-지붕올리기() // 10만줄
+땅파기(); // 10만줄
+기반다지기(); // 10만줄
+기둥세우기(); // 10만줄
+벽돌쌓기(); // 10만줄
+지붕올리기(); // 10만줄
 
 // 함수의 일반적인 형태
 // 함수 선언문
 // 파선아실
-function one(a, b) { // 1
-    let z = a + b // 2
-    return z ** 2 // 3
+function one(a, b) {
+  // 1
+  let z = a + b; // 2
+  return z ** 2; // 3
 }
 
-console.log(one(7, 3))
-console.log(one(7, 3))
+console.log(one(7, 3));
+console.log(one(7, 3));
 
 // 화살표 함수
-const two = (a, b) => (a + b) ** 2
-console.log(two(7, 3))
-console.log(two(7, 3))
+const two = (a, b) => (a + b) ** 2;
+console.log(two(7, 3));
+console.log(two(7, 3));
 
 // 이름 없이 선언하는 함수
 // 함수 표현식
-const three = function (a, b) { // 1
-    let z = a + b // 2
-    return z ** 2 // 3
-}
+const three = function (a, b) {
+  // 1
+  let z = a + b; // 2
+  return z ** 2; // 3
+};
 
-console.log(three(7, 3))
-console.log(three(7, 3))
+console.log(three(7, 3));
+console.log(three(7, 3));
 
 // 콜백함수
 // 함수를 아규먼트로 전달해서 언젠가는 사용해줄게!
 function four(a, b, c) {
-    let z = c(a, b) + c(a, b)
-    return z * 2
+  let z = c(a, b) + c(a, b);
+  return z * 2;
 }
 
-four(7, 3, one)
+four(7, 3, one);
 
 // 아래와 같이 했을 경우에는 함수의 순수성, 순수함수의 장점을 살릴 수 없습니다.
 // 외부에서 직접 값을 가져오는 것을 지양해주세요.
 function four(a, b, c) {
-    let z = one(a, b) + one(a, b)
-    return z * 2
+  let z = one(a, b) + one(a, b);
+  return z * 2;
 }
 
-four(7, 3, one)
-
+four(7, 3, one);
 
 // 함수
 // 읽어볼만한 문헌 : https://ko.javascript.info/function-basics
 // return, console.log를 헷갈려 하십니다.
 
 function hello(para) {
-    console.log(para)
-    console.log('hello')
-    return 100
+  console.log(para);
+  console.log("hello");
+  return 100;
 }
 
-console.log(hello(10))
+console.log(hello(10));
 
-let x = console.log('hello')
-x // undefined
+let x = console.log("hello");
+x; // undefined
 
 // 함수, 메서드(클래스 안에 들어간 함수)
 // .을 찍어 접근할 수 있는 함수다? => 메서드
 // 함수를 호출할 수 있는 이름은 결국 변수입니다.
-let x = console.log
-x('hello')
+let x = console.log;
+x("hello");
 
 // 2번(아래 3개는 같은 코드입니다.)
 function hello1() {
-    console.log('hello');
+  console.log("hello");
 }
 
 function hello2() {
-    console.log('hello');
-    return
+  console.log("hello");
+  return;
 }
 
 function hello3() {
-    console.log('hello');
-    return undefined
+  console.log("hello");
+  return undefined;
 }
 
-let a = hello1()
-let b = hello2()
-let c = hello3()
+let a = hello1();
+let b = hello2();
+let c = hello3();
 
 // return
 function hello4() {
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
-    return
-    console.log('hello')
-    console.log('hello')
-    console.log('hello')
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
+  return;
+  console.log("hello");
+  console.log("hello");
+  console.log("hello");
 }
 
-hello4()
+hello4();
 
 function hello5() {
-    if (true) {
-        if (false) { // 이 값을 바꿔보세요.
-            if (true) {
-                return
-            }
-        }
+  if (true) {
+    if (false) {
+      // 이 값을 바꿔보세요.
+      if (true) {
+        return;
+      }
     }
-    console.log('hello')
+  }
+  console.log("hello");
 }
 
 // return을 하더라도 1회 반복만 종료되는 것이지 전체 반복이 종료되는 것은 아닙니다.
 const x = [10, 20, 30, 40];
 x.forEach(function (el) {
-    console.log(el)
-    return
-    console.log('world')
+  console.log(el);
+  return;
+  console.log("world");
 });
-
 
 /////////////////////
 
 function 함수1(a, b, c) {
-    return a + b + c
+  return a + b + c;
 }
 
-함수1(10, 20, 30, 40) //error가 발생하지 않습니다.
+함수1(10, 20, 30, 40); //error가 발생하지 않습니다.
 
-함수1(10, 20) // error가 발생하지 않습니다.
+함수1(10, 20); // error가 발생하지 않습니다.
 
 function 함수1(a = 10, b = 20, c = 30) {
-    return a + b + c
+  return a + b + c;
 }
-함수1(1, 1)
+함수1(1, 1);
 
 function 함수1(a = 10, b = 20, c = 30) {
-    return a + b + c
+  return a + b + c;
 }
 // a와 c에 들어갈 것이라고 생각했지만 a와 b에 들어갑니다.
-함수1(a = 1, c = 1)
+함수1((a = 1), (c = 1));
 
 // 아래와 같은 식별 이슈가 있을 경우 object로 넘깁니다. roro기법
-function runPython(user, time, code, lv) {
+function runPython(user, time, code, lv) {}
+runPython("leehojun", 100, "function...", 3);
 
-}
-runPython('leehojun', 100, 'function...', 3)
-
-
-function runPython({ user, time, code, lv }) {
-
-}
+function runPython({ user, time, code, lv }) {}
 
 runPython({
-    user: 'leehojun',
-    time: 100,
-    code: 'function...',
-    lv: 3
-})
+  user: "leehojun",
+  time: 100,
+  code: "function...",
+  lv: 3,
+});
 
 // 기본값 설정
-function runPython({
-    user = '',
-    time = 0,
-    code = '',
-    lv = 0 }) {
-}
-
+function runPython({ user = "", time = 0, code = "", lv = 0 }) {}
 
 //////////////////
 
@@ -202,18 +194,18 @@ function runPython({
 // 읽어볼만한 문헌 : https://ko.javascript.info/arrow-functions-basics
 
 function 함수1(x, y) {
-    return x + y
+  return x + y;
 }
 // 위 함수를 화살표 함수로 작성하면 아래와 같습니다.
-let 함수1 = (x, y) => x + y
+let 함수1 = (x, y) => x + y;
 
 // 만악 함수 실행시 전달하는 인자가 한 개라면 소괄호를 생략할 수 있습니다.
-let 함수2 = x => {
-    return x + 10
-}
+let 함수2 = (x) => {
+  return x + 10;
+};
 
 // 화살표 함수 내부에서 한 줄 표현식만 반환한다면 return 키워드를 생략해도 됩니다.
-let 함수3 = x => x + 10
+let 함수3 = (x) => x + 10;
 
 let 결과 = 함수3(2);
 
@@ -221,10 +213,56 @@ console.log(결과);
 
 // 즉시 실행 함수
 (function () {
-    console.log('이 함수는 만들어지자마자 바로 실행됩니다!');
+  console.log("이 함수는 만들어지자마자 바로 실행됩니다!");
 })();
 
-function 함수() {
+function 함수() {}
+
+함수();
+
+// 4월 12일 코드
+
+function 함수(a, b) {
+  return a + b;
 }
 
-함수()
+// 어떻게 사용하고 원리는 무엇인가요?
+function 로그인정보({
+  회원등급 = "Gold",
+  글쓰기 = true,
+  글읽기 = true,
+  채널관리 = true,
+  백업 = "1주일 이내 가능",
+  소셜로그인여부 = ture,
+}) {
+  console.log("...함수 기능...");
+}
+
+// 원리(모르셔도 코딩하는데 지장있진 않습니다.)
+let one;
+let two;
+let three;
+
+let four = { one, two, three };
+// {one: undefined, two: undefined, three: undefined}
+// 이렇게 사용할 수 있게 된 것이 최근입니다.
+// '이렇게'는 변수 값을 object안에 넣으면 변수명이 key가되고 변수의 값이 object의 value가 되는 문법을 얘기합니다.
+
+let { one, two, three } = { one: 10, two: 20, three: 30 };
+let { one, two, three } = { two: 20, one: 10, three: 30 };
+
+// 살짝 심화
+// 위 코드에서 로그인정보({}) 호출 했을 때에는 error가 안나지만 로그인정보()호출했을 경우에는 error가 납니다.
+function 함수({ a = 1, b = 2, c = 3 } = {}) {
+  console.log(a, b, c);
+  return a + b + c;
+}
+
+함수();
+함수({});
+함수({ b: 100 });
+
+// 참고삼아서만 알아두세요.
+// let {one = 1, two = 2} = {one:100}
+// let {one = 1, two = 2} = {}
+// let {a=10, b=20, c=30} = undefined
